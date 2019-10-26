672a7e556dd2ae94f8174df8ee4fedef6cf291ee
Mark Bastian <markbastian@gmail.com>
2019-10-25 23:55:34 -0600

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject markbastian/partsbin "0.1.2"
+(defproject markbastian/partsbin "0.1.3-SNAPSHOT"
   :description "A project for creating functional, data-driven systems."
   :url "https://github.com/markbastian/partsbin"
   :license {:name "Eclipse Public License"
